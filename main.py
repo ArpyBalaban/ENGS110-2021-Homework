1f4154e5bfd4445471a7877dfa8ed18a3188acb2
Debian <debian@debian10.linuxvmimages.local>
2021-02-22 04:05:50 -0600

    homework assignment in python

---
+++ main.py
@@ -0,0 +1,11 @@
+age =int(input("please, type your age: "))
+f1 = 0
+f2 = 1
+sum = 1
+while (f2 <= age):
+    sum = sum + f2
+    temp = f1 + f2
+    f1 = f2
+    f2 = temp
+    print(temp)
+print(sum)
